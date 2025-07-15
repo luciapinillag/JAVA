f07a97e3b88f7105768cdcea633017b352a26207
Lucia Pinilla <lucia.pinilla.gonzalez@gmail.com>
2025-07-14 21:29:53 -0400

    colecciones agregados

--- POO-Proyectos/src/Coleccion.java
+++ POO-Proyectos/src/Coleccion.java
@@ -14,6 +14,25 @@ public class Coleccion {
         autos.add("Toyota");
         System.out.println(autos);
         System.out.println("auto en la posicion 2: " + autos.get(2));
+
+        //listar todos los autos utilizando get(i)
+        System.out.println(" ");
+        for (int i = 0; i < autos.size(); i++){
+            System.out.println(autos.get(i));
+
+        }
+
+        //actualizar un elemento en la posicion 2
+        autos.set(2, "Chevrolet");
+
+        //listamos la lista actualizada
+        System.out.println(" ");
+        for (int i = 0; i < autos.size(); i++){
+            System.out.println(autos.get(i));
+
+        }
+
+
     }
 
 }
